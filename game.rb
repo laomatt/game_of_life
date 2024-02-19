5dad3d66370c1236faabd12fe53cc34c7d7ce414
Matt L <matt@Matts-MacBook-Pro-2.local>
2024-02-18 16:09:36 -0800

    improve display

--- game.rb
+++ game.rb
@@ -1,4 +1,5 @@
 require 'colorize'
+require 'sequel'
 
 class NilClass
 	def [] arg
@@ -8,15 +9,16 @@ end
 
 class GameBoard
 	def initialize(rank)
-		@char = "*"
 		@log = {}
 		@rank = rank
+		@step = 0
+		@disp_char = '███'
 
 		@current_matrix = []
 		rank.times do |a|
 			row = []
 			rank.times do |b|
-				row << "[   ]"
+				row << " "
 			end
 			@current_matrix << row
 		end
@@ -33,11 +35,11 @@ class GameBoard
 			row = []
 			column_counter = 0
 			@rank.times do |b|
-				if @current_matrix[row_counter][column_counter].include?(@char)|| (
+				if @current_matrix[row_counter][column_counter] == 1|| (
 						(column_counter == random_column_placement) && 
 						(row_counter == random_row_placement)
 					)
-					row << "#{@char}"
+					row << "#{1}"
 				else
 					row << " "
 				end
@@ -55,7 +57,7 @@ class GameBoard
 	def display(first=false)
 		output = ""
 		@current_matrix.each do |row|
-			output += "#{row.join(' ')}" + "\n"
+			output += "#{row.map { |e| e == 1 ? @disp_char : @disp_char.gsub(/./) { |match| ' '  } }.join('')}" + "\n"
 		end
 
 		if first
@@ -70,16 +72,15 @@ class GameBoard
 		# Any live cell with two or three live neighbors lives on to the next generation.
 		# Any live cell with more than three live neighbors dies, as if by overpopulation.
 		# Any dead cell with exactly three live neighbors becomes a live cell, as if by reproduction.
+		@step += 1
 
 		new_matrix = []
-		@log[@current_matrix.clone] = true
-
+		@log[@current_matrix.clone] = @step
 
 		@current_matrix.each_with_index do |row, y|
 			new_row = []
 			row.each_with_index do |cell, x|
 				count = 0
-				# find neighbors\[]
 				[
 					@current_matrix[y][x + 1],
 					@current_matrix[y][x - 1],
@@ -90,26 +91,26 @@ class GameBoard
 					@current_matrix[y - 1][x - 1],
 					@current_matrix[y - 1][x]
 				].compact.each do |neighbor|
-					if neighbor.include?(@char)
+					if neighbor == 1
 						count += 1
 					end
 				end
 
 				# decide fate
 				next_gen = begin
-					if cell.include?(@char)
+					if cell == 1
 						if count < 2
-							" "
+							0
 						elsif count == 2 || count == 3
-							"#{@char}"
+							1
 						else
-							" "
+							0
 						end
 					else
 						if count == 3
-							"#{@char}"
+							1
 						else
-							" "
+							0
 						end
 					end
 				end
@@ -132,11 +133,8 @@ class GameBoard
 	end
 
 	def initialize_board amt
-		amt.times do 
-			print  "\r" + ("\e[A\e[K"*@rank)
-			input_matrix
-			display(true)
-		end
+		@current_matrix.map! { |row| row.map { rand(2) } }
+		display(true)
 
 		print "\n"
 		print "\n"
@@ -151,13 +149,15 @@ class GameBoard
 			sleep(0.1)
 
 			if @log[@current_matrix.clone] 
-				print "finished in #{@log.length} steps \n"
+				print "Finished in #{@log.length} steps.  Loop begins at step: #{@log[@current_matrix.clone]} \n"
 				break 
 			end
 		end
 	end
 end
 
-b = GameBoard.new(25)
-b.initialize_board rand(625)
+input_length = ARGV[0].to_i
+
+b = GameBoard.new(input_length)
+b.initialize_board rand(input_length*input_length)
 b.play
